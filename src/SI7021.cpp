dc2fd7ed21890e0c24e703fc03cde77302fe6f59
endail <52652357+endail@users.noreply.github.com>
2021-09-16 14:17:23 +1000

    .

--- src/SI7021.cpp
+++ src/SI7021.cpp
@@ -330,7 +330,7 @@ void SI7021::refresh() {
 
     this->_i2cMultiRead(
         _CMD_REGS.at(Command::MEASURE_HUM_HOLD_MASTER),
-        std::extent<decltype(_CMD_REGS.at(Command::MEASURE_HUM_HOLD_MASTER))>::value,
+        sizeof(_CMD_REGS.at(Command::MEASURE_HUM_HOLD_MASTER)) / sizeof(_CMD_REGS.at(Command::MEASURE_HUM_HOLD_MASTER)[0]),
         data,
         sizeof(data));
 
@@ -358,7 +358,7 @@ void SI7021::refresh() {
 
     this->_i2cMultiRead(
         _CMD_REGS.at(Command::READ_TEMP_FROM_PREV_HUM_MEASURE),
-        std::extent<decltype(_CMD_REGS.at(Command::READ_TEMP_FROM_PREV_HUM_MEASURE))>::value,
+        sizeof(_CMD_REGS.at(Command::READ_TEMP_FROM_PREV_HUM_MEASURE)) / sizeof(_CMD_REGS.at(Command::READ_TEMP_FROM_PREV_HUM_MEASURE)[0]),
         data,
         sizeof(data));
 
